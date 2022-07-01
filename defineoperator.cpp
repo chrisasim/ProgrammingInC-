0ae2dcd9b51368cccf7b8b34ae6f337320920f16
Christodoulos Asiminidis <43860123+chrisasim@users.noreply.github.com>
2022-07-01 17:23:09 +0300

    Introduction in C++

---
+++ defineoperator.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+
+using namespace std;
+
+class sample
+{
+ char *s;
+
+ public:
+ sample();
+ sample (const sample &ob);
+ ~sample(); //destructor
+ void show();
+ void set(char *str);
+ sample operator=(sample &ob);
+};
+
+sample::sample()
+{
+ s = new char('\0');
+}
+
+
+sample::sample(const sample &ob)
+{
+ s = new char[strlen(ob.s) + 1];
+ strcpy(s, ob.s);
+}
+
+void sample::set(char *str)
+{
+ s = new char[strlen(str) +1];
+ strcpy(s, str);
+}
+sample sample::operator=(sample &ob)
+{
+ if (strlen(ob.s)> strlen(s))
+ {
+  delete[] s;
+  s = new char[strlen(ob.s) + 1];
+ }
+ strcpy(s, ob.s);
+ return *this;
+}
+
+sample input()
+{
+ char instr[80];
+ sample str;
+ cout<<"Enter a string:";
+ cin>>instr;
+ str.set(instr);
+ return str;
+}
+
+int main()
+{
+ sample ob;
+ ob = input();
+ ob.show();
+ return 0;
+}
